b2a0455b9e9b697fec371c692bf170c1ffc5d08a
Luiz Estácio <luizstacio@gmail.com>
2015-05-26 21:04:52 -0300

    Fix(Ref Schema)

--- core/controller.js
+++ core/controller.js
@@ -1,106 +1,96 @@
-var Schema;
+function init (Schema) {
+  /**
+  * Create new model
+  *
+  * @method create
+  * @param {Object} req Request object
+  * @param {Object} res Response object
+  * @param {Function} callback Callback function
+  */
+  function create (req, res, cb) {
+    var dados = req.body;
+    var model = new Schema(dados);
 
-/**
-* Initialize methods.
-*
-* @method init
-* @param {Object} Schema expects mongoose.schema.
-* @return {Object} Methods of schema.
-*/
-function init (schema) {
-  Schema = schema;
+    model.save(function (err, data) {
+      cb(err, data, res);
+    });
+  }
+
+  /**
+  * Retrive all models in collection
+  *
+  * @method retrieve
+  * @param {Object} req Request object
+  * @param {Object} res Response object
+  * @param {Function} callback Callback function
+  */
+  function retrieve (req, res, cb){
+    Schema.find({}, function (err, data) {
+      cb(err, data, res);
+    });
+  }
+
+  /**
+  * Retrive models in collection
+  *
+  * @method retrieveById
+  * @param {Object} req Request object
+  * @param {Object} res Response object
+  * @param {Function} callback Callback function
+  */
+  function retrieveById (req, res, cb){
+    var id = req.params.id;
+    var query = { _id: id };
+
+    Schema.find(query, function (err, data) {
+      cb(err, data, res);
+    });
+  }
+
+  /**
+  * Update model by id
+  *
+  * @method update
+  * @param {Object} req Request object
+  * @param {Object} res Response object
+  * @param {Function} callback Callback function
+  */
+  function update (req, res, cb){
+    var id = req.params.id;
+    var query = { _id: id }; 
+    var mod = req.body;
+
+    delete mod._id;
+    Schema.update(query, mod, function (err, data) {
+      cb(err, data, res);
+    });
+  }
+
+  /**
+  * Remove model by id
+  *
+  * @method remove
+  * @param {Object} req Request object
+  * @param {Object} res Response object
+  * @param {Function} callback Callback function
+  */
+  function remove (req, res, cb){
+    var id = req.params.id;
+    var query = {_id: id};
+
+    Schema.remove(query, function(err, data) {
+      cb(err, data, res);
+    });
+  }
 
   return {
     create: create,
     retrieve: retrieve,
     retrieveById: retrieveById,
     update: update,
-    remove: remove
+    remove: remove,
+    schema: Schema
   }
-}
-
-/**
-* Create new model
-*
-* @method create
-* @param {Object} req Request object
-* @param {Object} res Response object
-* @param {Function} callback Callback function
-*/
-function create (req, res, cb) {
-  var dados = req.body;
-  var model = new Schema(dados);
-
-  model.save(function (err, data) {
-    cb(err, data, res);
-  });
-}
-
-/**
-* Retrive all models in collection
-*
-* @method retrieve
-* @param {Object} req Request object
-* @param {Object} res Response object
-* @param {Function} callback Callback function
-*/
-function retrieve (req, res, cb){
-  Schema.find({}, function (err, data) {
-    cb(err, data, res);
-  });
-}
-
-/**
-* Retrive models in collection
-*
-* @method retrieveById
-* @param {Object} req Request object
-* @param {Object} res Response object
-* @param {Function} callback Callback function
-*/
-function retrieveById (req, res, cb){
-  var id = req.params.id;
-  var query = { _id: id };
-
-  Schema.find(query, function (err, data) {
-    cb(err, data, res);
-  });
-}
-
-/**
-* Update model by id
-*
-* @method update
-* @param {Object} req Request object
-* @param {Object} res Response object
-* @param {Function} callback Callback function
-*/
-function update (req, res, cb){
-  var id = req.params.id;
-  var query = { _id: id }; 
-  var mod = req.body;
-
-  delete mod._id;
-  Schema.update(query, mod, function (err, data) {
-    cb(err, data, res);
-  });
-}
-
-/**
-* Remove model by id
-*
-* @method remove
-* @param {Object} req Request object
-* @param {Object} res Response object
-* @param {Function} callback Callback function
-*/
-function remove (req, res, cb){
-  var id = req.params.id;
-  var query = {_id: id};
-
-  Schema.remove(query, function(err, data) {
-    cb(err, data, res);
-  });
 }
 
 /*
